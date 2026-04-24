b2e4623b4f20a63cf39eb22d7c50874fbeb1a6c6
AbhiFit007 <jatinn93@gmail.com>
2026-04-24 16:20:48 +0530

    Create config.js

---
+++ api/config.js
@@ -0,0 +1,55 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    workflowApiVersion: "1.1",
+    type: "REST",
+    metaData: {
+      icon: "https://via.placeholder.com/40",
+      category: "message"
+    },
+    lang: {
+      "en-US": {
+        name: "Test Custom Activity",
+        description: "Testing Custom Activity via Webhook"
+      }
+    },
+    arguments: {
+      execute: {
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: {
+          mobile: "{{Contact.Default.MobileNumber}}",
+          name: "{{Contact.Default.FirstName}}"
+        }
+      }
+    },
+    configurationArguments: {
+      applicationExtensionKey: "4711188c-e459-41bf-b1bc-197f35b1da51",
+      validate: {
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST"
+      },
+      publish: {
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST"
+      }
+    },
+    userInterfaces: {
+      configModal: {
+        height: 300,
+        width: 400,
+        url: "https://abhifit007.github.io/Test-Custom-Activity/index.html"
+      }
+    },
+    schema: {
+      arguments: {
+        execute: {
+          inArguments: [],
+          outArguments: []
+        }
+      }
+    }
+  });
+}
