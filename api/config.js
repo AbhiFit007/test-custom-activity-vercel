ea15c867d305d0f3fab21e4a948dfc0b7ffc5f79
AbhiFit007 <jatinn93@gmail.com>
2026-04-28 12:05:32 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-  return res.status(200).json({
+  res.status(200).json({
     workflowApiVersion: "1.1",
     type: "REST",
     metaData: {
@@ -8,30 +8,47 @@ export default function handler(req, res) {
     },
     lang: {
       "en-US": {
-        name: "Test Activity",
-        description: "Test"
+        name: "Test Custom Activity",
+        description: "Testing Custom Activity via Webhook"
       }
     },
     arguments: {
       execute: {
-        url: "https://test-custom-activity-vercel.vercel.app/api/config",
-        verb: "POST"
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: {
+          mobile: "{{Contact.Default.MobileNumber}}",
+          name: "{{Contact.Default.FirstName}}"
+        }
       }
     },
     configurationArguments: {
-      applicationExtensionKey: "6ede0c69-f2d1-4058-9583-a3955f2dcc9f",
-      publish: {
-        url: "https://test-custom-activity-vercel.vercel.app/api/config"
-      },
+      applicationExtensionKey: "8c2ead8b-9584-4e66-99d1-2afe4d2a827e", 
       validate: {
-        url: "https://test-custom-activity-vercel.vercel.app/api/config"
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST"
+      },
+      publish: {
+        url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
+        verb: "POST"
       }
     },
     userInterfaces: {
       configModal: {
-        height: 200,
-        width: 300,
-        url: "https://example.com"
+        height: 300,
+        width: 400,
+        url: "https://abhifit007.github.io/Test-Custom-Activity/index.html"
+      }
+    },
+    schema: {
+      arguments: {
+        execute: {
+          inArguments: [],
+          outArguments: []
+        }
       }
     }
   });
