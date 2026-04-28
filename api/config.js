f1ac4444efe7b0d06d8c946e4e2fbade6723aabd
AbhiFit007 <jatinn93@gmail.com>
2026-04-28 12:30:07 +0530

    Update config.js

--- api/config.js
+++ api/config.js
@@ -1,32 +1,25 @@
 export default function handler(req, res) {
   res.status(200).json({
     workflowApiVersion: "1.1",
-    type: "REST",
     metaData: {
       icon: "https://via.placeholder.com/40",
-      category: "message"
+      category: "custom"
     },
+    type: "REST",
     lang: {
       "en-US": {
-        name: "Test Custom Activity",
-        description: "Testing Custom Activity via Webhook"
+        name: "Test Activity",
+        description: "Test"
       }
     },
     arguments: {
       execute: {
         url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
-        verb: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: {
-          mobile: "{{Contact.Default.MobileNumber}}",
-          name: "{{Contact.Default.FirstName}}"
-        }
+        verb: "POST"
       }
     },
     configurationArguments: {
-      applicationExtensionKey: "8c2ead8b-9584-4e66-99d1-2afe4d2a827e", 
+      applicationExtensionKey: "8c2ead8b-9584-4e66-99d1-2afe4d2a827e",
       validate: {
         url: "https://webhook.site/d9ce391e-0752-4d0c-9988-df1c975ab4db",
         verb: "POST"
@@ -38,17 +31,9 @@ export default function handler(req, res) {
     },
     userInterfaces: {
       configModal: {
-        height: 300,
-        width: 400,
-        url: "https://abhifit007.github.io/Test-Custom-Activity/index.html"
-      }
-    },
-    schema: {
-      arguments: {
-        execute: {
-          inArguments: [],
-          outArguments: []
-        }
+        height: 200,
+        width: 300,
+        url: "https://example.com"
       }
     }
   });
